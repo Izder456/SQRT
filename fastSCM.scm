bc35dc120f810e36636ac84563e6300ceef6e0c0
izzy <izder456@disroot.org>
2023-08-14 02:34:56 -0500

    a little tweaking

--- fastSCM.scm
+++ fastSCM.scm
@@ -1,5 +1,7 @@
 ;; Load necessary libraries
 (import (srfi 27))  ; Library for bitwise operations
+(import (srfi 4)) ; library for u32vector
+(import (chicken random))
 
 ;; Define the Q_rsqrt function
 (define (Q-rsqrt number)
@@ -21,7 +23,7 @@
     ;; Perform bit manipulation on the integer representation of y
     (set-u32vector! int-y 0 (- (u32vector-ref int-y 0) (arithmetic-shift i -1)))
     ;; Convert the bytes back to a floating-point value
-    (set! y (bytes->single-float int-y))
+    (set! y (bytes->float int-y))
     ;; Perform the Quake inverse square root algorithm
     (set! y (* y (- threehalfs (* x2 (* y y)))))
     y))  ; Return the result of the algorithm
@@ -32,8 +34,8 @@
   (let loop ((i 0)  ; Counter for the loop
              (res 0.0))  ; Result storage
     (if (< i 1000000)  ; Check if the loop should continue
-        (let* ((r (random 4294967296))  ; Generate a random 32-bit integer
-               (q (Q-rsqrt (single-float r))))  ; Apply Q_rsqrt function
+        (let* ((r (random-integer 4294967296))  ; Generate a random 32-bit integer
+               (q (Q-rsqrt r)))  ; Apply Q_rsqrt function
           (loop (+ i 1) q))  ; Recursively continue the loop
         0)))  ; Return 0 when the loop is complete
 
